aa11bf85771488634d7bd1ec581a391201cd07c1
Vojtech Vitek <vojtech.vitek@pressly.com>
2017-05-16 11:46:29 -0400

    Implement trap functionality

---
+++ README.md
@@ -0,0 +1,21 @@
+trap: Trap and print signals.
+======
+
+# Usage
+```bash
+$ trap
+^Cinterrupt
+^Cinterrupt
+Killed: 9
+```
+
+# Installation
+
+```bash
+go get -u github.com/VojtechVitek/trap
+```
+*You might need to [download Go](https://golang.org/dl/) first.*
+
+# License
+
+Licensed under the [MIT License](./LICENSE).
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, signals...)
+
+	go func() {
+		for {
+			fmt.Printf("%s\n", <-sig)
+		}
+	}()
+
+	select {}
+}
+
+var signals = []os.Signal{
+	syscall.SIGABRT,
+	syscall.SIGALRM,
+	syscall.SIGBUS,
+	syscall.SIGCHLD,
+	//syscall.SIGCLD,
+	syscall.SIGCONT,
+	syscall.SIGFPE,
+	syscall.SIGHUP,
+	syscall.SIGILL,
+	syscall.SIGINT,
+	syscall.SIGIO,
+	syscall.SIGIOT,
+	syscall.SIGKILL,
+	syscall.SIGPIPE,
+	//syscall.SIGPOLL,
+	syscall.SIGPROF,
+	//syscall.SIGPWR,
+	syscall.SIGQUIT,
+	syscall.SIGSEGV,
+	//syscall.SIGSTKFLT,
+	syscall.SIGSTOP,
+	syscall.SIGSYS,
+	syscall.SIGTERM,
+	syscall.SIGTRAP,
+	syscall.SIGTSTP,
+	syscall.SIGTTIN,
+	syscall.SIGTTOU,
+	//syscall.SIGUNUSED,
+	syscall.SIGURG,
+	syscall.SIGUSR1,
+	syscall.SIGUSR2,
+	syscall.SIGVTALRM,
+	syscall.SIGWINCH,
+	syscall.SIGXCPU,
+	syscall.SIGXFSZ,
+}
